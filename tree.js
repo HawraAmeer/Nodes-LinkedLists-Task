73f6e5d68ff2405111d420f021f8e43c3ba8ee5c
HawraAmeer <hwra.ameer97@gmail.com>
2021-01-18 13:55:02 +0300

    update for myself

--- tree.js
+++ tree.js
@@ -10,9 +10,15 @@ class TreeNode {
     if (this.children.length < 2) {
       this.children.push(node);
       console.log(`added a child to ${node.name.split(" ")[1]}`);
+      // console.log(`${node.name} child of ${this.name}`);
     } else console.log(`child is an orphan`);
   };
 
+  //   getChildWithName = (name) => {
+  //     return this.children.find((child) => child.name === name);
+  //   };
+
+  // this was not there =)
   traverseToFindParent = (child) => {
     let nodes = [this];
     while (nodes.length > 0) {
@@ -37,6 +43,8 @@ class TreeNode {
 
 const root = new TreeNode("family");
 
+// const root = new TreeNode("AlKandery");
+
 let childName = prompt("enter child full name (done if finished):");
 while (childName !== "done") {
   let child = new TreeNode(childName);
@@ -47,5 +55,32 @@ while (childName !== "done") {
 
   childName = prompt("enter child full name (done if finished):");
 }
+
+// while (childName !== "done") {
+//   let current = root;
+
+//   let names = childName.split(" ").reverse();
+//   let firstName = names.pop();
+//   let lastName = names.shift();
+
+//   if (lastName === current.name) {
+//     if (names) {
+//       for (let name of names) {
+//         let child = current.getChildWithName(name);
+//         if (child) {
+//           current = child;
+//         } else { // Bonus: creates the in betweens nodes
+//           const newNode = new TreeNode(name);
+//           current.addChild(newNode);
+//           current = newNode;
+//         }
+//       }
+//     }
+//     const newNode = new TreeNode(firstName);
+//     current.addChild(newNode);
+//   }
+//   console.log("--------------------------------------------------");
+//   childName = prompt("enter child full name (done if finished):");
+// }
 
 root.traverse();
